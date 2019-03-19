f55feba8ad155db2f93b81fc96004dc718ad8e68
Elena I <elena.schykina@gmail.com>
2019-03-19 11:17:53 +0300

    module4-task1

--- src/make-films.js
+++ src/make-films.js
@@ -42,3 +42,19 @@ export const makeFilms = (films, isControl) => (
     ))
     .join(``)
 );
+
+export const makeFilm = (film, isControl) => (
+  `<article class="film-card ${isControl ? `` : `film-card--no-controls`}">
+    <h3 class="film-card__title">${film.title}</h3>
+    <p class="film-card__rating">${film.rating}</p>
+    <p class="film-card__info">
+      <span class="film-card__year">${film.year}</span>
+      <span class="film-card__duration">${film.duration}</span>
+      <span class="film-card__genre">${film.genre}</span>
+    </p>
+    <img src="${film.poster}" alt="" class="film-card__poster">
+    <p class="film-card__description">${film.description}</p>
+    <button class="film-card__comments">${film.commentsCount} comments</button>
+    ${isControl ? makeControls() : ``}
+  </article>`
+);
